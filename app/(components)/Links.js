f4c6e03dc094906d881af7639bb3c980e0b109ba
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-03 10:17:38 -0700

    initial

--- app/(components)/Links.js
+++ app/(components)/Links.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Links(props){
     let result = []
     for (let elem in props.links){
-        result.push(<><Link href = {props.links[elem].href}> {props.links[elem].label} </Link> <br/></>)
+        result.push(<><a href = {props.links[elem].href}> {props.links[elem].label} </a> <br/></>)
     }
     return(
             <div className={"links"}>
