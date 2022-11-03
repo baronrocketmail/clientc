14c200ba8b6b74233e5c23997a571b4562c6a709
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-03 09:58:00 -0700

    initial

--- app/page.js
+++ app/page.js
@@ -1,16 +1,24 @@
 async function getName(){
-    return fetch("https://undefxx.com/api/info/name",  { next: { revalidate: 1 } })
+    return fetch("https://undefxx.com/api/info/name", { next: { revalidate: 1 } }).then(x=> x.json());
 }
 
 async function getUnpaid(){
-    return fetch("https://undefxx.com/api/payments/unpaid",  { next: { revalidate: 1 } })
+    return fetch("https://undefxx.com/api/payments/unpaid", { next: { revalidate: 1 } }).then(x=> x.json());
+}
+
+async function getApplicationsOpen(){
+    return fetch("https://undefxx.com/api/info/applicationsOpen", { next: { revalidate: 1 } }).then(x=> x.json());
 }
 
 export default async function Page(){
     const name = await getName()
     const unpaid = await getUnpaid()
+    const applicationsOpen = await getApplicationsOpen()
+
+    let links = []
+
 
     return(
-            <h1>929 lee ave</h1>
+            <h1>{typeof applicationsOpen}</h1>
     )
 }
