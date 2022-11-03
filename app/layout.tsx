715849aa00d9b7232d8e151040df162ae3220417
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-03 08:02:02 -0700

    initia

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import localFont from '@next/font/local';
 import './globals.css'
 
 
-const myFont = localFont({ src: './GoldmanSans_W_Th.woff2' , preload: true , variable: "--goldmanSansLight", display: "swap"});
+const myFont = localFont({ src: './GoldmanSans_W_Th.woff2' , preload: true , variable: "--goldmanSansLight", display: "swap", fallback: ["arial"]});
 
 export default function RootLayout({ children }: {
   children: React.ReactNode;
