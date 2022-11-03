86d73cb1dfada8badf506357597ced762132906b
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-03 11:32:11 -0700

    init

--- app/autopay/page.js
+++ app/autopay/page.js
@@ -1,13 +1,29 @@
 "use client";
 import Links from "../(components)/Links";
-import {use, useState} from "react"
+import {use, useEffect, useMemo, useState} from "react"
+import {propertyID} from "public/constants.mjs"
+import {initializeApp} from "firebase/app";
+import {db} from "../(components)/firestoreInit.js";
+import { doc, onSnapshot } from "firebase/firestore";
+
 
 async function getAutopayActive(){
     return fetch("https://undefxx.com/api/info/autopay", { next: { revalidate: 1 } }).then(x=> x.json());
 }
 
 export default function Page(){
-    const [autopayActive, setAutopayActive] = useState(use(getAutopayActive()))
+
+
+    useEffect(() => {
+        const unsub = onSnapshot(doc(db, "/units/18572 Cull Canyon Rd/info", "info"), (doc) => {
+            setAutopayActive(doc.data().autopay);
+            console.log("Current data: ", doc.data());
+        });
+    },[])
+
+//    const initialAutopayActive = use(getAutopayActive())
+
+    const [autopayActive, setAutopayActive] = useState(true)
 
 
     let links = [{label: "<---", href: "/"}, {label:  autopayActive ? ("autopay: active") : ("autopay: inactive"), href: "/"}]
@@ -19,3 +35,4 @@ export default function Page(){
             </>
     )
 }
+
