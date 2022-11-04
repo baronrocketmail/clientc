f335bbbbbb42eab16292e89d74fec6b6e404f14d
baronrocketmail <114305919+baronrocketmail@users.noreply.github.com>
2022-11-04 00:07:04 -0700

    Update Links.js

--- app/(components)/Links.js
+++ app/(components)/Links.js
@@ -5,7 +5,7 @@ export default function Links(props){
 
     let result = []
     for (let elem in props.links){
-        result.push(<><Link prefetch = {false} href = {props.links[elem].href}> {props.links[elem].label} </Link> <br/></>)
+        result.push(<><Link href = {props.links[elem].href}> {props.links[elem].label} </Link> <br/></>)
     }
     return(
             <div className={"links"}>
